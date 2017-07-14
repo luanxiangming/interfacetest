4f67fb9c2bca269e6df35f791b8788514e4e64fa
Oliver Luan <xiangming.luan@gmail.com>
2017-07-14 17:36:26 +0800

    mysql

--- src/main/java/com/vipabc/interfacetest/utils/ConnectToDB.java
+++ src/main/java/com/vipabc/interfacetest/utils/ConnectToDB.java
@@ -11,102 +11,105 @@ import java.util.logging.Logger;
 
 public class ConnectToDB {
 
-	private Connection conn;
+    private Connection conn;
 
-	public ConnectToDB() {
-		String driver = "com.mysql.cj.jdbc.Driver";
+    public ConnectToDB() {
+        String driver = "com.mysql.cj.jdbc.Driver";
 
-		String url = "jdbc:mysql://172.16.233.82:3306/vfs";
+        String url = "jdbc:mysql://172.16.233.82:3306/vfs";
 
-		String user = "vfsdev";
+        String user = "vfsdev";
 
-		String password = "vfs123";
+        String password = "vfs123";
 
-		try {
-			Class.forName(driver);
-			conn = DriverManager.getConnection(url, user, password);
-			if (!conn.isClosed())
-				System.out.println("Succeeded connecting to the Database!");
-		} catch (ClassNotFoundException e) {
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, user, password);
+            if (!conn.isClosed())
+                System.out.println("Succeeded connecting to the Database!");
+        } catch (ClassNotFoundException e) {
 
-			System.out.println("Sorry,can't find the Driver!");
-			e.printStackTrace();
+            System.out.println("Sorry,can't find the Driver!");
+            e.printStackTrace();
 
-		} catch (SQLException e) {
+        } catch (SQLException e) {
 
-			e.printStackTrace();
+            e.printStackTrace();
 
-		} catch (Exception e) {
+        } catch (Exception e) {
 
-			e.printStackTrace();
+            e.printStackTrace();
 
-		}
-	}
+        }
+    }
 
-	public List<HashMap<String, String>> query(String table, String key) throws SQLException {
-		Statement statement = conn.createStatement();
+    public List<HashMap<String, String>> query(String table, String key) throws SQLException {
+        Statement statement = conn.createStatement();
         ResultSet rs = statement.executeQuery("select * from " + table);
         List<HashMap<String, String>> result = new ArrayList<HashMap<String, String>>();
 
         ResultSetMetaData md = rs.getMetaData();
         int cc = md.getColumnCount();
-		while (rs.next()) {
+        while (rs.next()) {
             HashMap<String, String> columnMap = new HashMap<String, String>();
             for (int i = 1; i <= cc; i++) {
                 columnMap.put(md.getColumnName(i), rs.getString(i));
             }
             result.add(columnMap);
-		}
-		rs.close();
-		conn.close();
-		return result;
-	}
+        }
+        rs.close();
+        conn.close();
+        return result;
+    }
 
-	public  List<HashMap<String, String>> query(String sql) throws SQLException {
-		Statement statement = conn.createStatement();
-		ResultSet rs = statement.executeQuery(sql);
-		List<HashMap<String, String>> result = new ArrayList<HashMap<String, String>>();
+    public List<HashMap<String, String>> query(String sql) throws SQLException {
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery(sql);
+        List<HashMap<String, String>> result = new ArrayList<HashMap<String, String>>();
 
         ResultSetMetaData md = rs.getMetaData();
         int cc = md.getColumnCount();
-		while (rs.next()) {
+        while (rs.next()) {
             HashMap<String, String> columnMap = new HashMap<String, String>();
             for (int i = 1; i <= cc; i++) {
                 columnMap.put(md.getColumnName(i), rs.getString(i));
             }
             result.add(columnMap);
-		}
-		rs.close();
+        }
+        rs.close();
 //        conn.close();
         return result;
-	}
+    }
 
-	public void insert(String sql) throws SQLException {
-		Statement statement = conn.createStatement();
-		statement.executeUpdate(sql);
-	}
-	public void update(String sql) throws SQLException {
-		Statement statement = conn.createStatement();
-		statement.executeUpdate(sql);
-	}
-	public void delete(String sql) throws SQLException {
-		Statement statement = conn.createStatement();
-		statement.executeUpdate(sql);
-	}
-	  public void closeDBcon(){
-	        try {
-	            conn.close();
-	        } catch (SQLException ex) {
-	             Logger.getLogger(Oracle.class.getName()).log(Level.SEVERE, null, ex);
-	        }
-	      }
+    public void insert(String sql) throws SQLException {
+        Statement statement = conn.createStatement();
+        statement.executeUpdate(sql);
+    }
 
-	public static void main(String args[]) throws SQLException {
-		ConnectToDB cdb = new ConnectToDB();
-		cdb.query("visit_identitify_code", "code_value");
-		cdb.query("visit_identitify_code", "status");
+    public void update(String sql) throws SQLException {
+        Statement statement = conn.createStatement();
+        statement.executeUpdate(sql);
+    }
+
+    public void delete(String sql) throws SQLException {
+        Statement statement = conn.createStatement();
+        statement.executeUpdate(sql);
+    }
+
+    public void closeDBcon() {
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Oracle.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String args[]) throws SQLException {
+        ConnectToDB cdb = new ConnectToDB();
+        cdb.query("visit_identitify_code", "code_value");
+        cdb.query("visit_identitify_code", "status");
 //		cdb.insert("INSERT INTO visit_identitify_code"
 //				+ "   (`id`, `creater`, `create_time`, `modifier`, `modify_time`, `version`, `code_value`, `follow_id`, `status`, `client_linkman_id`)"
 //				+ "VALUES   (3, -1, '2014/5/29 17:00:00', NULL, NULL, 0, '4321', 1, 'visiting', 1);");
-	}
+    }
 }
